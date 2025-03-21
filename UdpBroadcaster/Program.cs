b7029b6e813f775aec163ebb3c054231dc3571e1
DJ Helmick <dj.helmick@aristocrat.com>
2025-03-21 09:15:35 -0700

    Add project files.

---
+++ UdpBroadcaster/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UdpBroadcaster
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
